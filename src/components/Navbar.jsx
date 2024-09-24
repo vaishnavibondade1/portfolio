6a3a30d288bdc2e1c50e3e035d4f98f61fcf6bcc
Nico Robin <luffy@Saitama>
2024-09-24 20:16:41 +0530

    navbar hover change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
         >
           <ul className="flex items-center gap-1 py-2 text-lg">
             {menuLinks?.map((menu, i) => (
-              <li key={i} className="px-6 hover:text-cyan-600">
+              <li key={i} className="px-6 hover:text-gray-200">
                 <a href={menu?.link}>{menu?.name}</a>
               </li>
             ))}
