1615c05535611ea2df9901e1392eb88a6ddd932d
Nico Robin <luffy@Saitama>
2024-09-24 19:29:01 +0530

    eight commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   return (
     <nav
       className={`fixed w-full left-0 top-0 z-[999] ${
-        sticky ? "bg-cyan-600/60  text-gray-900" : "text-white"
+        sticky ? "bg-cyan-600/90  text-gray-900" : "text-white"
       }`}
     >
       <div className="flex items-center justify-between">
