8c277d235cb9466b8881feff8cd43ac0a7c9c5c1
Nico Robin <luffy@Saitama>
2024-09-24 19:34:53 +0530

    ninth commit

--- src/components/Bot.jsx
+++ src/components/Bot.jsx
@@ -15,12 +15,20 @@ const Bot = () => {
     }
   };
 
+  const menuLinks = [
+    { name: "HOME", link: "#home" },
+    { name: "ABOUT", link: "#about" },
+    { name: "SKILLS", link: "#skills" },
+    { name: "PROJECTS", link: "#projects" },
+    { name: "CONTACT", link: "#contact" },
+  ]; 
+
   return (
     <>
       <div
         aria-label="Open chat"
         role="button"
-        className="fixed bottom-5 sm:right-8 right-4 z-[999] cursor-pointer text-white text-4xl bg-cyan-600 w-16 h-16 flex items-center justify-center rounded-full animate-bounce"
+        className="fixed bottom-5 sm:right-8 right-4 z-[999] cursor-pointer text-white text-4xl bg-cyan-600 w-16 h-10 flex items-center justify-center rounded-full animate-bounce"
         onClick={toggleChat}
       >
         <ion-icon name="chatbubble-ellipses"></ion-icon>
@@ -30,16 +38,17 @@ const Bot = () => {
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -20 }}
-          className="fixed bottom-20 sm:right-8 right-4 z-[998] bg-white shadow-lg w-80 h-80 rounded-lg p-4 border border-gray-300"
+          className="fixed bottom-20 sm:right-8 right-4 z-[998] bg-white shadow-lg w-80 h-50 rounded-lg p-4 border border-gray-300"
         >
-          <h2 className="text-xl font-semibold">Chat with Us!</h2>
+          <h2 className="text-xl font-semibold">Hi! How can i help?</h2>
           <textarea
-            className="w-full h-40 p-2 border border-gray-300 rounded"
+            className="w-full h-20 p-2 border border-gray-300 rounded bg-gray-100"
             placeholder="Type your message..."
           />
-          <button className="mt-2 bg-cyan-600 text-white px-4 py-2 rounded">
+          <a href="MENU">   <button className="mt-2 bg-cyan-600 text-white px-4 py-2 rounded">
             Send
-          </button>
+          </button></a>
+       
         </motion.div>
       )}
     </>
